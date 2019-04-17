c02a83c9ce6f7fd07b86570b3d5b73a2150f84b2
Joaquin Cruz <joacocruz6@gmail.com>
2019-04-17 16:23:40 -0400

    Installed Django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/joaquin/Desktop/U/Semestre VII/Ingenieria de Software/2019-1-Electronic-Operations/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
